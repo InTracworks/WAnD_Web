e91a75683169df70196cf26213e0d54b57ef3f54
Prabu <prabu38@gmail.com>
2018-05-08 20:41:50 +0530

    title change

--- Theme/basic/embed.php
+++ Theme/basic/embed.php
@@ -15,7 +15,7 @@
     <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Emoncms embed - <?php echo $route->controller.' '.$route->action.' '.$route->subaction; ?></title>
+        <title>ITW WAnD embed - <?php echo $route->controller.' '.$route->action.' '.$route->subaction; ?></title>
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black">
         <link rel="apple-touch-startup-image" href="<?php echo $path; ?>Theme/<?php echo $theme; ?>/ios_load.png">
